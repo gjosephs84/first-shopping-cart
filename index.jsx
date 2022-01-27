0e5f6e0a56f0288cc7664554cda4b07fff8779be
Gregory Josephs <greg.josephs@gmail.com>
2022-01-27 08:15:32 -0500

    added functionality to move fruit to cart

--- index.jsx
+++ index.jsx
@@ -12,8 +12,12 @@ function NavBar({ stockitems }) {
     let [name, num] = e.target.innerHTML.split(":"); // innerHTML should be format name:3
     // use newStock = stock.map to find "name" and decrease number in stock by 1
     // only if instock is >=  do we move item to Cart and update stock
+    if (num > 0) {
+      let newCart = [...cart, name];
+      setCart(newCart);
+    }
     let newStock = stock.map((item, index) => {
-      if (item.name == name) item.instock--;
+      if ((item.name == name) && (item.instock > 0)) item.instock--;
       return item;
     });
     setStock(newStock);
@@ -25,11 +29,20 @@ function NavBar({ stockitems }) {
       </Button>
     );
   });
+
+  const updatedCart = cart.map((item, index) => {
+    return (
+      <Button key={index}>{item}</Button>
+    )
+  });
+
+  
   // note that React needs to have a single Parent
   return (
     <>
       <ul style={{ listStyleType: "none" }}>{updatedList}</ul>
       <h1>Shopping Cart</h1>
+      <ul style={{ listStyleType: "none"}}>{updatedCart}</ul>
     </>
   );
 }
